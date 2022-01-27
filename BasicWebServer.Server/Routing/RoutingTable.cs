5a6953c704855b941b0fe8bef29e89330be8a152
Sava Ivanov <tie.mail@abv.bg>
2022-01-27 17:00:15 +0200

    BeforeMVC

--- BasicWebServer.Server/Routing/RoutingTable.cs
+++ BasicWebServer.Server/Routing/RoutingTable.cs
@@ -19,10 +19,11 @@ namespace BasicWebServer.Server.Routing
 
         public RoutingTable() => this.routes = new()
         {
-            [Method.Get] = new(),
-            [Method.Post] = new(),
-            [Method.Put] = new(),
-            [Method.Delete] = new(),
+            // StringComparer.InvariantCultureIgnoreCase  <-- игнорира главните букви
+            [Method.Get] = new(StringComparer.InvariantCultureIgnoreCase),
+            [Method.Post] = new(StringComparer.InvariantCultureIgnoreCase),
+            [Method.Put] = new(StringComparer.InvariantCultureIgnoreCase),
+            [Method.Delete] = new(StringComparer.InvariantCultureIgnoreCase)
         };
 
         public IRoutingTable Map(string url, Method method, Response response) => method switch
