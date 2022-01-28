3c20c4e1e70bd211da8927f7e7c0d850ccf5c4c8
Sava Ivanov <tie.mail@abv.bg>
2022-01-28 21:27:37 +0200

    MVCupToCoolies

--- BasicWebServer.Demo/Controllers/HomeController.cs
+++ BasicWebServer.Demo/Controllers/HomeController.cs
@@ -5,16 +5,106 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BasicWebServer.Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+            Name: <input type='text' name='Name'/>
+            Age: <input type='number' name ='Age'/>
+            <input type='submit' value ='Save' />
+        </form>";
+
+        private const string DownloadForm = @"<form action='/Content' method='POST'>
+            <input type='submit' value ='Download Sites Content' /> 
+        </form>";
+
+        private const string FileName = "content.txt";
+
         public HomeController(Request request) : base(request)
         {
         }
 
         // .MapGet("/", new TextResponse("Hello from the server!"))
         public Response Index() => Text("Hello from the server!");
+        public Response Redirect() => Redirect("https://softuni.org");
+        public Response Html() => Html(HomeController.HtmlForm);
+        public Response HtmlFromPost()
+        {
+            string formData = string.Empty;
+
+            foreach (var (key, value) in Request.Form)
+            {
+                formData += $"{key} - {value}";
+                formData += Environment.NewLine;
+            }
+
+            return Text(formData);
+        }
+        public Response Content() => Html(HomeController.DownloadForm);
+        public Response DownloadContent()
+        {
+            DownloadSitesAsTextFile(
+                HomeController.FileName,
+                new string[] { "https://judge.softuni.org/", "https://softuni.org/" })
+                .Wait();
+
+            return File(HomeController.FileName);
+        }
+
+
+        // the DownloadWebSiteContent(string url) method, which should get the first 2000 symbols
+        // of the HTML content of a site on a given URL
+        private static async Task<string> DownloadWebSiteContent(string url)
+        {
+            // To get the content, we should send a "GET" request to the site and read its content.
+            // For this reason, we will use the HttpClient class, which sends HTTP requests and receives HTTP responses
+            // from a resource, identified by a URL. The class provides us with the GetAsync(string requestUri)
+            // and the ReadAsStringAsync() methods. 
+
+            var httpClient = new HttpClient();
+            using (httpClient)
+            {
+                var response = await httpClient.GetAsync(url);
+
+                var html = await response.Content.ReadAsStringAsync();
+
+                // At the end, return only part of the HTML content, so that the result file is not too big
+                return html.Substring(0, 2000);
+            }
+        }
+
+        
+        private static async Task DownloadSitesAsTextFile(string filename, string[] urls)
+        {
+            // a collection of type Task<string>, which holds the tasks for getting the HTML content from the sites
+            // свалянето е асинхронно. в Лист-а ще добавяме Таскове. Забележи, че отпред няма await (въпреки, че мотода е async)
+            // изчакването на тези Такове става при var responses = await Task.WhenAll(downloads); (след foreach-a)
+            var downloads = new List<Task<string>>();
+
+            foreach (var url in urls)
+            {
+                downloads.Add(DownloadWebSiteContent(url));
+            }
+
+
+            // Напрактика още със стартирането на сървъра се изпълнява този метод т.е. изтегля си информация от 
+            // двата сайта и се "напълва" File
+            await DownloadSitesAsTextFile(HomeController.FileName,
+                new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
+
+            // Wait for all tasks to be executed together (in parallel) and get the result like this:
+            // WhenAll - значи, че ще изчакаме да приключат всички Таскове, които са е Лист-а (downloads e Лист)
+            string[] responses = await Task.WhenAll(downloads);
+
+            // Join all the content from the responses in a way you want and get the result.
+            var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
+
+            // Finally, use the File class to write the HTML content of the sites to a file with a given name asynchronously
+            //File.WriteAllTextAsync(filename, responsesString);
+        }
+
     }
 }
--- BasicWebServer.Demo/Startup.cs
+++ BasicWebServer.Demo/Startup.cs
@@ -10,20 +10,20 @@ namespace BasicWebServer.Demo
 {
     public class Startup
     {
-        // Create the form as a string constant and use it as a part of an HTML response
-        private const string HtmlForm = @"<form action='/HTML' method='POST'>
-            Name: <input type='text' name='Name'/>
-            Age: <input type='number' name ='Age'/>
-            <input type='submit' value ='Save' />
-        </form>";
+        //// Create the form as a string constant and use it as a part of an HTML response
+        //private const string HtmlForm = @"<form action='/HTML' method='POST'>
+        //    Name: <input type='text' name='Name'/>
+        //    Age: <input type='number' name ='Age'/>
+        //    <input type='submit' value ='Save' />
+        //</form>";
 
         // това е html-a, когато искаме да даунлодваме ->
         // имаме Даунлод бутон за да тригърнем the action on the "/Content" page with method "POST":
-        private const string DownloadForm = @"<form action='/Content' method='POST'>
-            <input type='submit' value ='Download Sites Content' /> 
-        </form>";
+        //private const string DownloadForm = @"<form action='/Content' method='POST'>
+        //    <input type='submit' value ='Download Sites Content' /> 
+        //</form>";
 
-        private const string FileName = "content.txt";
+        //private const string FileName = "content.txt";
 
         private const string LoginForm = @"<form action='/Login' method='POST'>
             Username: <input type='text' name='Username'/>
@@ -39,23 +39,24 @@ namespace BasicWebServer.Demo
         {
             // Напрактика още със стартирането на сървъра се изпълнява този метод т.е. изтегля си информация от 
             // двата сайта и се "напълва" File
-            await DownloadSitesAsTextFile(Startup.FileName,
-                new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
+            //await DownloadSitesAsTextFile(Startup.FileName,
+            //    new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
 
-            await new HttpServer(routes => routes
-                .MapGet<HomeController>("/", c => c.Index()))  // връщаме съответен екшън, за целта се ползва се делегат
-                //.MapGet<HomeController>("/Redirect", c => c.Redirect())
-                //.MapGet<HomeController>("/HTML", c => c.Html())
-                //.MapPost<HomeController>("/HTML", c => c.HtmlFromPost())
-                //.MapGet<HomeController>("/Content", c => c.Content())
-                //.MapPost<HomeController>("/Content", c => c.DownloadContent())
-                //.MapGet<HomeController>("/Cookies", c => c.Cookies())
-                //.MapGet<HomeController>("/Session", c => c.Session()))
-                ////.MapGet<HomeController>("/Login", new HtmlResponse(Startup.LoginForm))
-                ////.MapPost<HomeController>("/Login", new HtmlResponse("", Startup.LoginAction))
-                ////.MapGet<HomeController>("/Logout", new HtmlResponse("", Startup.LogoutAction))
-                ////.MapGet<HomeController>("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction)))
-                .Start();
+            var server = new HttpServer(routes => routes
+                .MapGet<HomeController>("/", c => c.Index())  // връщаме съответен екшън, за целта се ползва се делегат
+                .MapGet<HomeController>("/Redirect", c => c.Redirect())
+                .MapGet<HomeController>("/HTML", c => c.Html())
+                .MapPost<HomeController>("/HTML", c => c.HtmlFromPost())
+                .MapGet<HomeController>("/Content", c => c.Content())
+                .MapPost<HomeController>("/Content", c => c.DownloadContent())
+                .MapGet<HomeController>("/Cookies", c => c.Cookies()));
+            //.MapGet<HomeController>("/Session", c => c.Session()))
+            ////.MapGet<HomeController>("/Login", new HtmlResponse(Startup.LoginForm))
+            ////.MapPost<HomeController>("/Login", new HtmlResponse("", Startup.LoginAction))
+            ////.MapGet<HomeController>("/Logout", new HtmlResponse("", Startup.LogoutAction))
+            ////.MapGet<HomeController>("/UserProfile", new HtmlResponse("", Startup.GetUserDataAction)))
+
+            await server.Start();   
         }
 
         private static void GetUserDataAction(Request request, Response response)
@@ -174,68 +175,68 @@ namespace BasicWebServer.Demo
             response.Body = bodyText;
         }
 
-        // ето така we can add an action to be executed before the response is returned
-        // т.е. we will add the form data to the response body:
-        private static void AddFormDataAction(Request request, Response response)
-        {
-            response.Body = "";
+        //// ето така we can add an action to be executed before the response is returned
+        //// т.е. we will add the form data to the response body:
+        //private static void AddFormDataAction(Request request, Response response)
+        //{
+        //    response.Body = "";
 
-            foreach (var (key, value) in request.Form)
-            {
-                response.Body += $"{key} - {value}";
-                response.Body += Environment.NewLine;
-            }
-        }
+        //    foreach (var (key, value) in request.Form)
+        //    {
+        //        response.Body += $"{key} - {value}";
+        //        response.Body += Environment.NewLine;
+        //    }
+        //}
 
-        // the DownloadWebSiteContent(string url) method, which should get the first 2000 symbols
-        // of the HTML content of a site on a given URL
-        private static async Task<string> DownloadWebSiteContent(string url)
-        {
-            // To get the content, we should send a "GET" request to the site and read its content.
-            // For this reason, we will use the HttpClient class, which sends HTTP requests and receives HTTP responses
-            // from a resource, identified by a URL. The class provides us with the GetAsync(string requestUri)
-            // and the ReadAsStringAsync() methods. 
+        //// the DownloadWebSiteContent(string url) method, which should get the first 2000 symbols
+        //// of the HTML content of a site on a given URL
+        //private static async Task<string> DownloadWebSiteContent(string url)
+        //{
+        //    // To get the content, we should send a "GET" request to the site and read its content.
+        //    // For this reason, we will use the HttpClient class, which sends HTTP requests and receives HTTP responses
+        //    // from a resource, identified by a URL. The class provides us with the GetAsync(string requestUri)
+        //    // and the ReadAsStringAsync() methods. 
 
-            var httpClient = new HttpClient();
-            using (httpClient)
-            {
-                var response = await httpClient.GetAsync(url);
+        //    var httpClient = new HttpClient();
+        //    using (httpClient)
+        //    {
+        //        var response = await httpClient.GetAsync(url);
 
-                var html = await response.Content.ReadAsStringAsync();
+        //        var html = await response.Content.ReadAsStringAsync();
 
-                // At the end, return only part of the HTML content, so that the result file is not too big
-                return html.Substring(0, 2000);
-            }
-        }
+        //        // At the end, return only part of the HTML content, so that the result file is not too big
+        //        return html.Substring(0, 2000);
+        //    }
+        //}
 
-        // As we have created the "POST" request mapping to return a TextFileResponse with a file name,
-        // we should fill in the file with data before the response is returned. To do this, we will create the
-        // DownloadSitesAsTextFile(…) method in the Startup class and invoke it before the mappings are created.
-        // With this method, we will get the HTML content of several sites and write it to a text file with a given name.
-        //  - the DownloadSitesAsTextFile() method, will downloads the content of given sites asynchronously.
-        //  - Then, the whole HTML content will be joined and written to a single file.  
-        //  - First, the method should accept a file name and a string array with URLs of sites to be downloaded:
-        private static async Task DownloadSitesAsTextFile(string filename, string[] urls)
-        {
-            // a collection of type Task<string>, which holds the tasks for getting the HTML content from the sites
-            // свалянето е асинхронно. в Лист-а ще добавяме Таскове. Забележи, че отпред няма await (въпреки, че мотода е async)
-            // изчакването на тези Такове става при var responses = await Task.WhenAll(downloads); (след foreach-a)
-            var downloads = new List<Task<string>>();
+        //// As we have created the "POST" request mapping to return a TextFileResponse with a file name,
+        //// we should fill in the file with data before the response is returned. To do this, we will create the
+        //// DownloadSitesAsTextFile(…) method in the Startup class and invoke it before the mappings are created.
+        //// With this method, we will get the HTML content of several sites and write it to a text file with a given name.
+        ////  - the DownloadSitesAsTextFile() method, will downloads the content of given sites asynchronously.
+        ////  - Then, the whole HTML content will be joined and written to a single file.  
+        ////  - First, the method should accept a file name and a string array with URLs of sites to be downloaded:
+        //private static async Task DownloadSitesAsTextFile(string filename, string[] urls)
+        //{
+        //    // a collection of type Task<string>, which holds the tasks for getting the HTML content from the sites
+        //    // свалянето е асинхронно. в Лист-а ще добавяме Таскове. Забележи, че отпред няма await (въпреки, че мотода е async)
+        //    // изчакването на тези Такове става при var responses = await Task.WhenAll(downloads); (след foreach-a)
+        //    var downloads = new List<Task<string>>();
 
-            foreach (var url in urls)
-            {
-                downloads.Add(DownloadWebSiteContent(url));
-            }
+        //    foreach (var url in urls)
+        //    {
+        //        downloads.Add(DownloadWebSiteContent(url));
+        //    }
 
-            // Wait for all tasks to be executed together (in parallel) and get the result like this:
-            // WhenAll - значи, че ще изчакаме да приключат всички Таскове, които са е Лист-а (downloads e Лист)
-            string[] responses = await Task.WhenAll(downloads);    
+        //    // Wait for all tasks to be executed together (in parallel) and get the result like this:
+        //    // WhenAll - значи, че ще изчакаме да приключат всички Таскове, които са е Лист-а (downloads e Лист)
+        //    string[] responses = await Task.WhenAll(downloads);    
 
-            // Join all the content from the responses in a way you want and get the result.
-            var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
+        //    // Join all the content from the responses in a way you want and get the result.
+        //    var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
 
-            // Finally, use the File class to write the HTML content of the sites to a file with a given name asynchronously
-            await File.WriteAllTextAsync(filename, responsesString);
-        }
+        //    // Finally, use the File class to write the HTML content of the sites to a file with a given name asynchronously
+        //    await File.WriteAllTextAsync(filename, responsesString);
+        //}
     }
 }
