2d193785c2f58110252009642172a624bac2864f
Sava Ivanov <tie.mail@abv.bg>
2022-01-29 23:25:37 +0200

    MVC fix Controller to return view

--- BasicWebServer.Server/Controllers/Controller.cs
+++ BasicWebServer.Server/Controllers/Controller.cs
@@ -3,6 +3,7 @@ using BasicWebServer.Server.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -37,5 +38,27 @@ namespace BasicWebServer.Server.Controllers
         protected Response NotFound() => new NotFoundResponse();
         protected Response Redirect(string location) => new RedirectResponse(location);
         protected Response File(string fileName) => new TextFileResponse(fileName);
+
+        // модификация на контролера за да може да връща вю. Ще има малко рефлекшън.
+        // за да можем да си свършим работата ни е нужно имато на вюто - имаме един атрибут: [CallerMemberName], който
+        // ни помага да вземем кой е извикал съответния метод т.е. "от кой метод е бил извикан този метод". Понеже имената
+        // на вютата и екшъните съвпадат (такава е конвенцията) т.е. ще търсим такива вюта, които съвпадат със съответния
+        // контролер. Обаче имаме малък проблем - ще трябва да се "поизчистят" имената където има "controller" т.е. трябва ни
+        // името на контролера, но без думичката "controller" в името -> за целта ще си направим метод GetControllerName().
+        // The GetControllerName() method gets the controller name, without the "Controller" part
+        // (for example "HomeController" --> "Home"):
+
+        // говорейки си за [CallerMemberName] -> викащия метод ще е някой от методите в HomeController или UsersController =>
+        // и името на вюто ще трябва да е същото (DownloadContent, Cookies, Session и т.н.).
+        // [CallerMemberName] e атрибут -> По принцип атрибута позволява да получим някаква допълнителна възножност ->
+        // в случея атрибута каза на компилатора от къде да си вземе това, което му трябва.
+        protected Response View([CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, GetControllerName()); // <-- ще се създаде нов ViewResponse
+
+        // това е метода, който маха "controller" името на контролера:
+        // По повод на контролера -> типа на нашия клас ще е някой от наследниците - примерно HomeController или UsersController
+        // и като премахнем от името думата "controller" - ще получим това, което ни трябва.
+        private string GetControllerName() => this.GetType().Name.Replace(nameof(Controller), string.Empty);
+        // .GetType() - ще върне тип на контролера в който се намираме в момента 
     }
 }
---
+++ BasicWebServer.Server/Responses/ViewResponse.cs
@@ -0,0 +1,41 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Responses
+{
+    internal class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResponse(string viewName, string controllerName) 
+            : base("", ContentType.Html)
+        {
+            // трябва да проверим как е структурирано името на вюто - защото е възможно да бъде подаен целия път до вюто,
+            // а може да бъде подадено и само името на вюто (в url-то). Целта е да вземем (To get) the HTML from our views,
+            // we will need the full path to them. By convention, each view has the .cshtml file extension and is accessed on:
+            // "…/Views/{controllerName}/{viewName}.cshtml". 
+            if (!viewName.Contains(PathSeparator))
+            {
+                // т.е. ако във viewName няма PathSeparator - ще си конструираме съответния в viewName така:
+                viewName = controllerName + PathSeparator + viewName;
+            }
+            
+            // след като сме уточнили viewName-a -> можем да си билднем ПЪЛНИЯ път до нашето вю:
+            var viewPath = Path.GetFullPath($"./Views/" + viewName.TrimStart(PathSeparator) + ".cshtml");
+            // Path.GetFullPath --> Path e системна функция, а GetFullPath е някакъв метод на тази функция
+            // . <-- точката означава текуща директория. т.е. търсим от текущата директория - папка, която се казва вю
+            // след което залепваме viewName-a: viewName.TrimStart(PathSeparator)... и най-накрая залепваме разширенито .cshml
+
+            // вече можем да изчетем самия контент
+            // Finally, we should read the view file content as a text and add it to the response body:
+            var viewContent = File.ReadAllText(viewPath);
+
+            // виж родителя : ContentResponse
+            Body = viewContent;
+        }
+    }
+}
